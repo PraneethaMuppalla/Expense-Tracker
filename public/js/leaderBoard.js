94de907aede3791a341e0bb76ee8c79a02afd79c
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-12-20 22:01:06 +0530

    Leader Board

--- public/js/leaderBoard.js
+++ public/js/leaderBoard.js
@@ -6,25 +6,30 @@ const axiosInstance = axios.create({
 });
 
 const tbodyEl = document.getElementById("tbody");
-function renderEachExpense(each) {
+function renderEachExpense(each, i) {
   let tr = document.createElement("tr");
   tr.id = `rowEl${each.id}`;
-
   let td1 = document.createElement("td");
-  td1.textContent = each.totalExpenses;
+  td1.textContent = i;
 
   let td2 = document.createElement("td");
-  td2.textContent = each.user.name;
+  td2.textContent = each.totalExpenses;
+
+  let td3 = document.createElement("td");
+  td3.textContent = each.user.name;
 
   tr.appendChild(td1);
   tr.appendChild(td2);
-
+  tr.appendChild(td3);
   tbodyEl.appendChild(tr);
 }
 
 async function getLeaderBoard() {
   try {
     const result = await axiosInstance.get("/premium/getLeaderBoard");
+    result.data.map((each, i) => {
+      renderEachExpense(each, i);
+    });
   } catch (err) {
     console.error(err);
     errorToast(
